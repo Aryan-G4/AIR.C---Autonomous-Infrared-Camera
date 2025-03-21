b57498c2499a022f6503d38c59dc1f3bac64fac9
cool-sexy-guy-100 <ghosh.ary@gmail.com>
2025-03-21 11:20:33 -0400

    added code to read all registers
    -we have determined the camera is defective and have ordered a new one

--- Transmitter/project-name/main/main.c
+++ Transmitter/project-name/main/main.c
@@ -6,7 +6,11 @@
 #include "driver/gpio.h"
 #include "driver/i2c.h"
 
+#define NUM_ROWS 24
+#define NUM_COLS 32
 
+/*Important registers*/
+#define STATUS_REG 0x8000
 
 #define UART_NUM UART_NUM_0  // Use UART2
 #define TXD_PIN 17           // GPIO17 as TX
@@ -42,18 +46,42 @@ void app_main() {
     printf("testing printf");
     
     while (1) {
-        print_msg("hi\n");
+        //print_msg("hi\n");
         //uart_write_bytes(UART_NUM, message, strlen(message)); // Send message over UART
 
         uint16_t pixelval = 1;
-        pixelval = i2c_read(0x058A);
+        pixelval = i2c_read(0x0486);
         
-        sprintf(message, "i2c read completed, pixel value is: %x\n",pixelval);
+        sprintf(message, "i2c read completed, pixel value is: 0x%x\n",pixelval);
         print_msg(message);
+
+        print_msg("STARTING TO LOOK THROUGH ARRAY\n");
+        for(uint16_t i =0; i < 0x07FF; i+=4){
+            uint16_t regdata = i2c_read(i);
+            sprintf(message, "register %x value :%x\n",i,regdata);
+            print_msg(message);
+        }
+        print_msg("DONE reading registers\n");
+        //while(1);
+        /*Write to status register to clear bit*/
+        uint16_t cur_status = i2c_read(0x8000);
+        uint16_t new_status = cur_status & 0xFFF7;
+        i2c_write(new_status,0x8000);
+
+        /*Set start bit*/
+         cur_status = i2c_read(0x8000);
+         new_status = cur_status | 0x0020;
+        i2c_write(new_status,0x8000); 
+
+        while((i2c_read(STATUS_REG) & 0x0008) == 0){
+            print_msg("waiting for status bit\n");
+            vTaskDelay(100);
+        }
+
         //vTaskDelay(10000);
         //i2c_write(0x0001,0x8000);
         gpio_set_level(LED_PIN, 1);  // Turn LED ON
-        vTaskDelay(pdMS_TO_TICKS(5000)); // Delay 1 second
+        vTaskDelay(pdMS_TO_TICKS(3000)); // Delay 1 second
         gpio_set_level(LED_PIN, 0);  // Turn LED OFF
     }
 }
@@ -92,33 +120,32 @@ void print_msg(char* message){
 }
 
 uint16_t i2c_read(uint16_t reg){
-    print_msg("starting i2c read\n");
+    //print_msg("starting i2c read\n");
     uint8_t buffer[2] = {(reg&0xFF00)>>8, reg & 0x00FF};
     uint8_t result[2];
-    print_msg("entering i2c master write\n");
+    //print_msg("entering i2c master write\n");
     if(i2c_master_write_to_device(I2C_MASTER_PORT,DEVICE_ADDR,buffer,2,5000) != ESP_OK){
         print_msg("i2c master write failed\n");
     }
-    print_msg("completed i2c master write\n");
-    
+    //print_msg("completed i2c master write\n");
+    vTaskDelay(pdMS_TO_TICKS(10));
     if(i2c_master_read_from_device(I2C_MASTER_PORT,DEVICE_ADDR,result,2,5000) != ESP_OK){
         print_msg("i2c master read failed\n");
     }
-    print_msg("finished I2c read from reg\n");
-    uint16_t end = result[0] | result[1]<<8;
-    return end;
+    //print_msg("finished I2c read from reg\n");
+    return ((uint16_t)result[0] << 8) | result[1];
 
 }
 
 void i2c_write(uint16_t data,uint16_t reg) {
-    print_msg("starting i2c write\n");
-    uint8_t buffer[4] = {(reg&0xFF00)>>8, reg & 0x00FF, (data&0xFF00)>>8, data & 0x00FF};   // buffer contains address and data to be written
+    //print_msg("starting i2c write\n");
+    uint8_t buffer[4] = {(reg>>8)&0xFF, reg & 0x00FF, (data>>8)&0xFF, data & 0x00FF};   // buffer contains address and data to be written
 
-    print_msg("entering 12c master write\n");
+    //print_msg("entering 12c master write\n");
     if(i2c_master_write_to_device(I2C_MASTER_PORT, DEVICE_ADDR, buffer, 4, 5000)!=ESP_OK){
         print_msg("i2c master read failed\n");
     };
-    print_msg("completed i2c master write\n");
+    //print_msg("completed i2c master write\n");
 }
 
 void print_arr(int *arr, int rows, int cols) {
