f715fa797b1eaf55cde37c6948fe382c07a28554
cool-sexy-guy-100 <ghosh.ary@gmail.com>
2025-03-26 11:40:21 -0400

    Added Arduino library project and pulled Arduino.h

---
+++ Transmitter/MLX_Arduino_integration/main/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "Arduino.h"
+
+/*Things to change from the example code to make this work:
+
+1. Change any "serial. functions (i.e Serial.begin and Serial.println) to printf() or Uart transmission"app_main
+2. Remove void startup() and void loop() and instead put them into void app_main() with a while(1) loop.app_main
+
+*/
+void app_main(void)
+{
+
+}
